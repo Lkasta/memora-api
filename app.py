f1774b5a5fd4fd8f1897d387c13b9407d1da0a67
Lkasta <lkastabackup@gmail.com>
2025-08-24 00:59:00 -0300

    feat: test deploy

--- app.py
+++ app.py
@@ -12,10 +12,16 @@ def create_app():
   with app.app_context():
     db.create_all()
 
+  # Rota de teste simples
+  @app.route("/")
+  def home():
+    return "<h1>Backend rodando!</h1>"
+
+  # Registrar blueprint
   app.register_blueprint(memory_bp, url_prefix="/memories")
 
   return app
 
 if __name__ == "__main__":
   app_instance = create_app()
-  app_instance.run(debug=True)
+  app_instance.run(host="0.0.0.0", port=5000, debug=True)
