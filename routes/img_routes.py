c4f5c1e41ef9d3aa065b2d759b9739357747f152
Lkasta <lkastabackup@gmail.com>
2025-09-16 23:52:33 -0300

    feat(images-route): implement update image

--- routes/img_routes.py
+++ routes/img_routes.py
@@ -1,11 +1,11 @@
-from flask import Blueprint, request, Response
+from flask import Blueprint, request, Response, jsonify
 from werkzeug.utils import secure_filename
 
 from models import db, Image
 
 img_bp = Blueprint("imgs", __name__)
 
-@img_bp.route("/upload-img", methods=["POST"])
+@img_bp.route("/", methods=["POST"])
 def upload():
   pic = request.files['pic']
   user_id = request.form.get("user_id")
@@ -26,7 +26,7 @@ def upload():
 
   return "Image uploaded!", 200
 
-@img_bp.route('/')
+@img_bp.route('/', methods=["GET"])
 def get_img():
   user_id = request.form.get("user_id")
   memorie_id = request.form.get("memorie_id")
@@ -36,3 +36,31 @@ def get_img():
     return 'Img Not Found!', 404
 
   return Response(img.img, mimetype=img.mimetype)
+
+@img_bp.route("/", methods=["PUT"])
+def upload_img():
+  new_pic = request.files['pic']
+  new_filename = secure_filename(new_pic.filename)
+  new_mimetype = new_pic.mimetype
+
+  if not new_pic:
+    return "No image uploaded!", 400
+
+  user_id = request.form.get("user_id")
+  memorie_id = request.form.get("memorie_id")
+
+  img = Image.query.filter_by(user_id=user_id, memorie_id=memorie_id).first()
+
+  if not img:
+    return "Memory not found!", 404
+  
+  img.filename = new_filename
+  img.mimetype = new_mimetype
+  img.img = new_pic.read()
+
+  db.session.commit()
+
+  return jsonify({
+    "message": "Image updated successfully!",
+    "memory": {"id": memorie_id}
+  })
