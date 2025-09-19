ddb959b7fc447c9812c86c4124f56b6b5eecaff8
Lkasta <lkastabackup@gmail.com>
2025-09-19 15:19:06 -0300

    feat(user-routes): updated data fields validation

--- routes/user_routes.py
+++ routes/user_routes.py
@@ -3,22 +3,33 @@ from models import db, User
 
 user_bp = Blueprint("user", __name__)
 
-@user_bp.route("/", methods=["PUT"])
-def upload_user():
+@user_bp.route("/<int:user_id>", methods=["PUT"])
+def upload_user(user_id):
   data = request.json
-  user_id = data["user_id"]
-  print(f"user_id: {user_id}")
+
+  if not data:
+    return jsonify({"error": "No data provided"}), 400
 
   user = User.query.filter_by(id=user_id).first()
 
   if not user:
     return "User not fount!", 404
+  
+  username = data.get("username")
+  lastname = data.get("lastname")
+  email = data.get("email")
+  password = data.get("password")
 
-  if data["username"]:
+  if username:
     user.username = data["username"]
-  if data["lastname"]:
+  
+  if lastname:
     user.lastname = data["lastname"]
-  if data["password"]:
+  
+  if email:
+    user.email = data["email"]
+  
+  if password:
     user.set_password(data["password"])
 
   db.session.commit()
