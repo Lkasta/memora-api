0c0b1e4616ccca6523d21b19169c0eab83a8b533
Lkasta <lkastabackup@gmail.com>
2026-05-25 13:56:35 -0300

    feat(routes): register image and user image blueprints in the application

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,7 @@
 from .base import db
 from .user import User
 from .memory import Memory
+from .image import Image
+from .user_image import UserImage
 
-__all__ = ["db", "User", "Memory"]
+__all__ = ["db", "User", "Memory", "Image", "UserImage"]
